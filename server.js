fd8549935a8f9992e9cf9dac85f14961ee8b780e
Daniel Perdomo <cdkjarkas@gmail.com>
2020-05-06 12:01:38 -0500

    update

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-var express = require('express');
-var app = express();
-app.use(express.static("angular-tour-of-heroes"));
-app.get('/', function (req, res,next) {
- res.redirect('/'); 
-});
-app.listen(8080, 'localhost');
-console.log("MyProject Server is Listening on port 8080");
+// OpenShift sample Node application
+var express = require('express'),
+    app     = express(),
+    morgan  = require('morgan');
+    
+Object.assign=require('object-assign')
+
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
+
+
+app.use(express.static(__dirname + '/dist/angular-tour-of-heroes'));
+
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
